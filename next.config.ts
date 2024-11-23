1d23be1aa1c6fbec5a687033e6cf78019dd6a154
jargoh999 <151412083+jargoh999@users.noreply.github.com>
2024-11-23 10:31:07 +0100

    commit

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ module.exports = {
               source: '/api/:path*',
               headers: [
                   { key: 'Access-Control-Allow-Origin', value: 'http://localhost:5173', },
+                  { key: 'Access-Control-Allow-Origin', value: 'http://localhost:3000', },
                   { key: 'Access-Control-Allow-Origin', value: 'http://127.0.0.1:5500', },
                   { key: 'Access-Control-Allow-Methods', value: 'GET, POST, PUT, DELETE, OPTIONS' },
                   { key: 'Access-Control-Allow-Headers', value: 'Content-Type, Authorization' },
